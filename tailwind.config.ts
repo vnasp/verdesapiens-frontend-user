a0b154fd369f53f4e22365c30c2ecb2799ff728e
Valentina <valentinamr@gmail.com>
2024-03-29 18:31:00 -0300

    Remove custom colour

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,11 +13,7 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
-      extend: {
-        colors: {
-          'custom-green': '#6dc72e',
-        },
-      },
+      extend: {},
     },
   },
   plugins: [],
