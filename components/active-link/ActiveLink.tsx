ef44ecaa1de93622f34502566b8df2e5712e192b
Valentina <valentinamr@gmail.com>
2024-03-29 18:30:00 -0300

    Add color links and FontAwesome for menu icons

--- components/active-link/ActiveLink.tsx
+++ components/active-link/ActiveLink.tsx
@@ -3,17 +3,21 @@
 import Link from "next/link";
 import style from "./ActiveLink.module.css"
 import { usePathname } from "next/navigation";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { IconProp } from '@fortawesome/fontawesome-svg-core'; // Importar el tipo correcto para la prop icon
 
 interface Props {
   path: string;
-  icon: JSX.Element; // Le paso un elemento <Icon/> en vez de string
+  icon: IconProp; // Actualizar el tipo de 'icon' para que coincida con lo esperado por FontAwesomeIcon
 }
-export const ActiveLink = ({ path, icon } : Props) => {
+
+export const ActiveLink = ({ path, icon }: Props) => {
 
   const pathName = usePathname();
 
   return (
-    <Link href={path} className= {`${style.link} ${ (pathName === path ) ? style['active-link'] : '' } `}>{icon}</Link>
-
+    <Link href={path} className={`${style.link} ${(pathName === path) ? style['active-link'] : ''} `}>
+      <FontAwesomeIcon icon={icon} />
+    </Link>
   )
 }
