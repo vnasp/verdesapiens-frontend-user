d6554e917d79c94c1853247efa8dd09f9de5683e
Valentina <valentinamr@gmail.com>
2024-03-29 18:31:13 -0300

    Update Navbar with FontAwesome icons

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,8 +1,10 @@
 import Link from "next/link";
 import { ActiveLink } from "../active-link/ActiveLink";
-import { CalendarDaysIcon, CalculatorIcon, ArrowPathIcon } from "@heroicons/react/24/outline";
 import { Pacifico } from "next/font/google";
 import Image from "next/image";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCalculator, faCalendar, faRecycle } from "@fortawesome/free-solid-svg-icons";
+import { faCalendarDays } from "@fortawesome/free-regular-svg-icons";
 
 //Google Fonts
 const pacifico = Pacifico({ 
@@ -11,19 +13,21 @@ const pacifico = Pacifico({
   display:"swap"
 });
 
+// Ajusta tu arreglo para usar directamente los iconos importados
 const navItems = [
-  { path: '/calculadora', icon: <CalculatorIcon /> },
-  { path: '/calendario', icon: <CalendarDaysIcon /> },
-  { path: '/informacion', icon: <ArrowPathIcon /> },
-]
+  { path: '/calculadora', icon: faCalculator },
+  { path: '/calendario', icon: faCalendarDays },
+  { path: '/informacion', icon: faRecycle },
+];
+
 
 export const Navbar = () => {
   return (
     <>
       <nav className="flex justify-between items-center p-4">
         <div className="w-1/3"></div>
-        <div className="w-1/3 flex justify-center items-center text-3xl text-green-700">
-          <div><Image src="/logo.webp" className="h-14 me-4" alt="Logo VerdeSapiens"/></div>
+        <div className="w-1/3 flex justify-center items-center text-3xl text-lime-500">
+          <div><Image src="/logo.webp" width={56} height={56} alt="Logo VerdeSapiens" className="me-4"/></div>
           <div><Link href="/"><h1 className={pacifico.className}>VerdeSapiens</h1></Link></div>
         </div>
         <div className="w-1/3 flex justify-end">
