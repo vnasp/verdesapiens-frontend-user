ddc83093909477a470baef0870356c9ab97f00a1
Valentina <valentinamr@gmail.com>
2024-04-05 23:16:11 -0300

    Update logo

--- components/navbar/Navbar.jsx
+++ components/navbar/Navbar.jsx
@@ -3,41 +3,67 @@ import { ActiveLink } from "../active-link/ActiveLink";
 import { Pacifico } from "next/font/google";
 import Image from "next/image";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCalculator, faCalendar, faRecycle } from "@fortawesome/free-solid-svg-icons";
+import {
+  faCalculator,
+  faCalendar,
+  faRecycle,
+} from "@fortawesome/free-solid-svg-icons";
 import { faCalendarDays } from "@fortawesome/free-regular-svg-icons";
 
 //Google Fonts
-const pacifico = Pacifico({ 
-  subsets: ['latin'],
+const pacifico = Pacifico({
+  subsets: ["latin"],
   weight: "400",
-  display:"swap"
+  display: "swap",
 });
 
 // Ajusta tu arreglo para usar directamente los iconos importados
 const navItems = [
-  { path: '/calculadora', icon: faCalculator, ariaLabel: 'Lee más sobre la calculadora para huertos' },
-  { path: '/calendario', icon: faCalendarDays, ariaLabel: 'Lee más sobre el calendario para huertos' },
-  { path: '/informacion', icon: faRecycle, ariaLabel: 'Lee más sobre datos de sustentabilidad para huertos' },
+  {
+    path: "/calculadora",
+    icon: faCalculator,
+    ariaLabel: "Lee más sobre la calculadora para huertos",
+  },
+  {
+    path: "/calendario",
+    icon: faCalendarDays,
+    ariaLabel: "Lee más sobre el calendario para huertos",
+  },
+  {
+    path: "/informacion",
+    icon: faRecycle,
+    ariaLabel: "Lee más sobre datos de sustentabilidad para huertos",
+  },
 ];
-
 
 export const Navbar = () => {
   return (
     <>
-      <nav className="flex justify-between items-center p-4">
-        <div className="flex justify-between items-center text-3xl text-lime-500">
-          <div className="flex justify-start">
-            <Image src="/logo.webp" height={56} width={56} alt="Logo VerdeSapiens"/>
-            <Link href="/"><h1 className={`${pacifico.className} text-4xl shadow-lg`}>VerdeSapiens</h1></Link></div>
+      <nav className="flex justify-between items-center p-4 text-3xl text-lime-500">
+        <div className="flex justify-start items-center">
+          <Image
+            src="/logo.png"
+            height={56}
+            width={56}
+            alt="Logo VerdeSapiens"
+          />
+          <Link href="/">
+            <h1 className={`${pacifico.className} text-4xl shadow-lg ms-6`}>
+              VerdeSapiens
+            </h1>
+          </Link>
         </div>
         <div className="w-1/3 flex justify-end">
-          {
-            navItems.map(navItem => (
-              <ActiveLink key={navItem.path} path={navItem.path} icon={navItem.icon} ariaLabel={navItem.ariaLabel}/>
-            ))
-          }
+          {navItems.map((navItem) => (
+            <ActiveLink
+              key={navItem.path}
+              path={navItem.path}
+              icon={navItem.icon}
+              ariaLabel={navItem.ariaLabel}
+            />
+          ))}
         </div>
       </nav>
-      </>
-  )
-}
+    </>
+  );
+};
