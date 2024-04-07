4198e584cb10e525768d44d18c9917a7d3cca951
Valentina <valentinamr@gmail.com>
2024-04-06 23:49:20 -0300

    Add styles to slider

--- app/(general)/informacion/Slider.jsx
+++ app/(general)/informacion/Slider.jsx
@@ -5,7 +5,9 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
-import sliderData from '../../../data/sliderData';
+import dataSlider from '../../../data/dataSlider';
+import Image from "next/image";
+
 
 
 export const Slider = () => {
@@ -19,13 +21,20 @@ export const Slider = () => {
       onSwiper={(swiper) => console.log(swiper)}
       onSlideChange={() => console.log('slide change')}
     >
-      {sliderData.map((slide, index) => (
-        <SwiperSlide key={index}>
-          <div className="px-24">
+      {dataSlider.map((slide, index) => (
+        <SwiperSlide key={index} className="px-24 py-10">
             <h3>{slide.title}</h3>
-            <p className="py-4 text-justify">{slide.content}</p>
-            <p className="py-4 text-justify  bg-yellow-200 box__shadow-md p-4 border-0 rounded-3xl">{slide.content2}</p>
-          </div>
+            <section className="flex flex-col justify-center items-center gap-4">
+            <p className="text-justify">{slide.content}</p>
+            <Image
+                  src={`/icons/${slide.icon}.png`}
+                  width={225}
+                  height={150}
+                  alt={slide.title}
+                />
+           
+            <p className="py-4 text-justify bg-yellow-200 box__shadow-md p-4 border-0 rounded-2xl">{slide.content2}</p>
+            </section>
         </SwiperSlide>
       ))}
     </Swiper>
