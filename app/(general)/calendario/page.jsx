e646c49dbe65c7edad6c4e4f11347cd574839ffd
Valentina <valentinamr@gmail.com>
2024-04-06 23:49:55 -0300

    Improve semantic

--- app/(general)/calendario/page.jsx
+++ app/(general)/calendario/page.jsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import CalendarItem from "./calendar";
-import { activitiesCalendar } from "../../../data/activitiesCalendar";
+import dataCalendar from "../../../data/dataCalendar";
 
 export default function CalendarPage() {
   return (
@@ -16,27 +16,29 @@ export default function CalendarPage() {
           content="calendario, siembra, cosecha, podas, huertos"
         />
       </Head>
-      <h2 className="text-center">Calendario para Huertos</h2>
-      <section className="flex justify-between gap-12 p-4">
-        <div className="w-1/4 relative">
-          <div className="absolute bg-yellow-200 box__shadow-md p-4 border-0 rounded-3xl top-0 -left-20">
-            <h3>¿De qué trata?</h3>
-            <p className="mb-4">
-              Este calendario ofrece una guía sobre las fechas óptimas para
-              sembrar semillas, cosechar hortalizas, podar hierbas y realizar
-              actividades esenciales de mantenimiento del huerto.
-            </p>
-            <p className="mb-4">
-              Se han tenido en cuenta las particularidades climáticas y
-              estacionales del hemisferio sur para determinar las
-              recomendaciones.
-            </p>
-          </div>
+    <main>
+    <h2 className="text-center">Calendario para Huertos</h2>
+    <section className="flex justify-between gap-12 p-4">
+      <article className="w-1/4">
+        <div className="bg-yellow-200 box__shadow-md p-4 border-0 rounded-2xl md:ml-[-6rem]">
+          <h3>¿De qué trata?</h3> 
+          <p className="mb-4">
+            Este calendario ofrece una guía sobre las fechas óptimas para
+            sembrar semillas, cosechar hortalizas, podar hierbas y realizar
+            actividades esenciales de mantenimiento del huerto.
+          </p>
+          <p className="mb-4">
+            Se han tenido en cuenta las particularidades climáticas y
+            estacionales del hemisferio sur para determinar las
+            recomendaciones.
+          </p>
         </div>
-        <div className="w-3/4">
-          <CalendarItem activities={activitiesCalendar} />
-        </div>
-      </section>
+      </article>
+      <div className="w-3/4">
+        <CalendarItem activities={dataCalendar} />
+      </div>
+    </section>
+  </main>
     </>
   );
 }
